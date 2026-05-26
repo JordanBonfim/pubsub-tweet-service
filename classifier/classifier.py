bcaa2d2ed760a8f6d440be69ac560c54d214eae3
Jordan <jordanduzabonfim@gmail.com>
2026-05-26 11:35:54 -0300

    classifier topic-exchange connection

--- classifier/classifier.py
+++ classifier/classifier.py
@@ -2,6 +2,8 @@ import pika
 import time
 import json
 
+
+    
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 
@@ -10,6 +12,144 @@ consume_channel.queue_declare(queue='work_queue', durable=True, arguments={'x-qu
 print('Waiting for tweets.')
 
 
+publish_channel = connection.channel()
+publish_channel.exchange_declare(exchange='tweets_topics',exchange_type='topic')
+
+
+def classify_tweet(dict_tweet):
+
+    topics = []
+
+    author = dict_tweet["author"].lower()
+    content = dict_tweet["content"].lower()
+
+    # SPORTS
+    sports_words = [
+        "worldcup",
+        "soccer",
+        "football",
+        "fifa",
+        "goal",
+        "cup",
+        "lakers",
+        "golf",
+        "match",
+        "league",
+        "basketball"
+    ]
+
+    if author == "cristiano":
+        topics.append("sports")
+        topics.append("soccer")
+
+    for word in sports_words:
+        if word in content:
+            topics.append("sports")
+            break
+
+    # MUSIC
+    music_words = [
+        "music",
+        "song",
+        "album",
+        "playlist",
+        "itunes",
+        "mixtape",
+        "concert",
+        "studio"
+    ]
+
+    singers = [
+        "selenagomez",
+        "jtimberlake",
+        "katyperry",
+        "ladygaga",
+        "justinbieber",
+        "rihanna"
+    ]
+
+    if author in singers:
+        topics.append("music")
+
+    for word in music_words:
+        if word in content:
+            topics.append("music")
+            break
+
+    # MOVIES
+    movie_words = [
+        "movie",
+        "film",
+        "trailer",
+        "disney",
+        "theaters",
+        "series",
+        "superhero"
+    ]
+
+    for word in movie_words:
+        if word in content:
+            topics.append("movies")
+            break
+
+    # TECHNOLOGY
+    tech_words = [
+        "twitter",
+        "api",
+        "system",
+        "downtime",
+        "tweets",
+        "android",
+        "github",
+        "developers",
+        "autopilot"
+    ]
+
+    if author in ["twitter", "google", "github", "tesla"]:
+        topics.append("technology")
+
+    for word in tech_words:
+        if word in content:
+            topics.append("technology")
+            break
+
+    # GAMES
+    games_words = [
+        "zelda",
+        "minecraft",
+        "counter-strike",
+        "tournament",
+        "patch",
+        "players"
+    ]
+
+    if author in ["nintendo", "riotgames", "steam", "playstation"]:
+        topics.append("games")
+
+    for word in games_words:
+        if word in content:
+            topics.append("games")
+            break
+
+    # SPACE
+    space_words = [
+        "mars",
+        "starship",
+        "spacex",
+        "falcon",
+        "nasa",
+        "hubble"
+    ]
+
+    if author in ["spacex", "nasa"]:
+        topics.append("space")
+
+    for word in space_words:
+        if word in content:
+            topics.append("space")
+            break
+
+    return list(set(topics))
 
 def callback(ch, method, properties, body):
 
@@ -27,11 +167,16 @@ def callback(ch, method, properties, body):
             "number_of_likes": tweet["numberOfLikes"],
             "number_of_shares": tweet["numberOfShares"]
         }
-        print(dict_tweet)
-    
+        
+        topics = classify_tweet(dict_tweet)
         
         
-        
+        for topic in topics:
+            publish_channel.basic_publish(
+                exchange='tweets_topics',
+                routing_key=topic,
+                body=(dict_tweet["author"]+" publicou: "+dict_tweet["content"])
+            )
     time.sleep(3)             
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
